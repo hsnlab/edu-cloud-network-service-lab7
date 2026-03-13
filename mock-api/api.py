158fa14857825935f1ffe7cb6159d33dff73cde8
Janos Czentye <czentye@tmit.bme.hu>
2026-03-13 11:37:25 +0100

    Adjust data loading in mockapi to FastAPI lifespan protocol

--- mock-api/api.py
+++ mock-api/api.py
@@ -1,24 +1,35 @@
+import contextlib
 import json
 import operator
 import pathlib
-from time import strptime
+import time
 
 import fastapi
 
-app = fastapi.FastAPI(title="RappelConsoMockAPI", version='0.1')
-data: list = json.loads(pathlib.Path("rappelconso-v2-gtin-espaces.json").read_text())
+DATA: list = []
+
+
+@contextlib.asynccontextmanager
+async def lifespan(_app: fastapi.FastAPI):
+    global DATA
+    DATA = json.loads(pathlib.Path("rappelconso-v2-gtin-espaces.json").read_text())
+    yield
+    del DATA
+
+
+app = fastapi.FastAPI(title="RappelConsoMockAPI", version='0.1', lifespan=lifespan)
 
 
 @app.get('/api/records')
 async def get_data(limit: int = 10, offset: int = 0, order_by: str = None, where: str = None):
     if where:
         wh = where.strip().split()
-        wh_date = strptime(wh[2].strip("'").split('T')[0], "%Y-%m-%d")
+        wh_date = time.strptime(wh[2].strip("'").split('T')[0], "%Y-%m-%d")
         wh_op = operator.gt if '>' in wh[1] else operator.lt if '<' in wh[1] else operator.eq
-        results = list(filter(lambda d: wh_op(strptime(d[wh[0]].strip("'").split('T')[0],
-                                                       "%Y-%m-%d"), wh_date), data))
+        results = list(filter(lambda d: wh_op(time.strptime(d[wh[0]].strip("'").split('T')[0],
+                                                            "%Y-%m-%d"), wh_date), DATA))
     else:
-        results = data
+        results = DATA
     if order_by:
         ob = order_by.split()
         results = sorted(results,
